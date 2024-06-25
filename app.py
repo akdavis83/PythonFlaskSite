dbabd990031954afdb9e784a421eb98c1da8f25b
Alan Davis <155010643+akdavis83@users.noreply.github.com>
2024-06-25 01:14:35 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {'message': 'Hello from Flask!'}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
